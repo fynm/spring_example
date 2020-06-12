968ea984ca037c7ed653d9eb591ef088980c1daa
Shawn Fong <shawnxsf@gmail.com>
2020-06-11 21:45:37 -0400

    Generated via Spring Initializr - 2.3.1Snapshot / Java 8 / Maven

---
+++ spring-example/src/test/java/com/javaPractice/spring/basics/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaPractice.spring.basics.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
